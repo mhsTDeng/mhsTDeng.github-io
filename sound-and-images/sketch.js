637987697d4adbb472b6f18280133f9339cd6642
mhsTDeng <48356564+mhsTDeng@users.noreply.github.com>
2019-04-26 08:12:25 -0400

    Update sketch.js

--- sound-and-images/sketch.js
+++ sound-and-images/sketch.js
@@ -8,9 +8,9 @@ myImage = loadImage('prettybackground.jpg');
 function setup() {
   // put setup code here
   createCanvas(1000,800);
-  image(myImage,100,100);
 }
 
 function draw() {
   // put drawing code here
+  image(myImage,100,100);
 }
