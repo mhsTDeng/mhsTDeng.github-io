31da5ad5a5dea811ada40711cd080795e8b1790f
mhsTDeng <48356564+mhsTDeng@users.noreply.github.com>
2019-05-08 09:04:31 -0400

    Update sketch.js

--- snowman-drawing-new/sketch.js
+++ snowman-drawing-new/sketch.js
@@ -1,4 +1,8 @@
-//Setup the canvas
+/*
+Snowman Drawing
+Created by Tina Deng
+Revised May 8th, 2019
+*/
 function setup() {
   // put setup code here
   //Sets the screen to be 640 pixels wide and 480 pixels high
@@ -7,13 +11,13 @@ function setup() {
 
 function draw() {
   // put drawing code here
-  //Create the ground
+ //Create the ground
   rect(0,350,400,50);
-  //Create the snowman body, copied from Khan Academy
-ellipse(200,300,150,150);
-ellipse(200,200,100,100);
-ellipse(200,130,75,75);
+ //Create the snowman body, copied from Khan Academy
+  ellipse(200,300,150,150);
+  ellipse(200,200,100,100);
+  ellipse(200,130,75,75);
 //Create the arms
-line(160,200,100,100);
-line(240,200,300,100);
+  line(160,200,100,100);
+  line(240,200,300,100);
 }
