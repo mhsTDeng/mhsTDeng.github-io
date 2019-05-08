e40a4f01cedd95855a55d98e4ae3325cd97647b4
mhsTDeng <48356564+mhsTDeng@users.noreply.github.com>
2019-05-08 08:47:20 -0400

    Update sketch.js

--- mouse-interaction/sketch.js
+++ mouse-interaction/sketch.js
@@ -7,7 +7,7 @@ function setup() {
 
 function draw() {
   //create mouse interaction
-  //when program is run, creates ellipses based on mouse position, as well as colours based on mouse position
+  //Create colours and ellipses based on mouse position
   fill(mouseX,mouseY,145);
   ellipse(mouseX,mouseY,mouseX,mouseY);
 
