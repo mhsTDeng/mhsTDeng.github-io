7d2f36ad3effcc804d88c6845d0bb1195a6073e1
mhsTDeng <48356564+mhsTDeng@users.noreply.github.com>
2019-05-08 09:15:33 -0400

    Update sketch.js

--- using-the-keyboard/sketch.js
+++ using-the-keyboard/sketch.js
@@ -1,3 +1,9 @@
+/*
+Using the Keyboard
+Created by Tina Deng
+Revised May 8th, 2019
+*/
+
 function setup() {
   // put setup code here
   background(255,255,255);
@@ -5,30 +11,32 @@ function setup() {
   noStroke();
 }
 
+//Change mode to set rectangles to the center
 function draw() {
   // put drawing code here
   rectMode(CENTER);
 }
 
+//Keyboard inputs
 function keyPressed() {
-if (keyCode === UP_ARROW) {
-background(62,171,182);
-fill(238,74,123);
-rect(width/2,height/2, 400,400);
+  if (keyCode === UP_ARROW) {
+    background(62,171,182);
+    fill(238,74,123);
+    rect(width/2,height/2, 400,400);
 }
-else if (keyCode === DOWN_ARROW) {
-  background(243,80,75);
-  fill(22,196,104);
-  rect(300,600,250,250);
+  else if (keyCode === DOWN_ARROW) {
+   background(243,80,75);
+    fill(22,196,104);
+    rect(300,600,250,250);
 }
-else if (keyCode === LEFT_ARROW){
-  background(74,94,238);
-  fill(243,226,75);
-  ellipse(930,500,120,120);
+  else if (keyCode === LEFT_ARROW){
+    background(74,94,238);
+    fill(243,226,75);
+   ellipse(930,500,120,120);
 }
-else if (keyCode === RIGHT_ARROW){
-  background(88,201,231);
-  fill(232,159,95);
-  ellipse(200,250,500,500);
+  else if (keyCode === RIGHT_ARROW){
+    background(88,201,231);
+    fill(232,159,95);
+    ellipse(200,250,500,500);
 }
 }
