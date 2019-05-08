728db7cf4fd47911f808af8b996d5c5d8284a2bc
mhsTDeng <48356564+mhsTDeng@users.noreply.github.com>
2019-05-08 09:09:47 -0400

    Update sketch.js

--- sound-and-images/sketch.js
+++ sound-and-images/sketch.js
@@ -1,21 +1,31 @@
+/*
+Sound and Images
+Created by Tina Deng
+Revised May 8th, 2019
+*/
+
+//Variables for image and sound
 let myImage;
 let mySound;
 
+//Preload image and sound
 function preload(){
-myImage = loadImage('prettybackground.jpg');
-mySound = loadSound('roblox-death-sound_1.mp3');
+  myImage = loadImage('prettybackground.jpg');
+  mySound = loadSound('roblox-death-sound_1.mp3');
 }
+
 function setup() {
   // put setup code here
   createCanvas(1000,800);
-
-
 }
+
+//Show image
 function draw() {
   // put drawing code here
   image(myImage,100,100);
 }
 
+//Assign an action to play sound
 function mousePressed() {
   mySound.play();
 }
